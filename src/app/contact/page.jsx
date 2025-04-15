dc6c07f80daa4198381f6f2968662eb7a015407c
muskanDaruka <darukamuskan@gmail.com>
2025-04-15 14:18:06 +0530

    added: hover css

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -36,42 +36,42 @@ function page() {
         />
         <div className='relative xl:pt-48 md:pt-48 pt-16 px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 xl:gap-8 font-montserrat place-items-center'>
           <div className='xl:space-y-6 space-y-2 mb-4'>
-            <h2 data-aos="fade-right" className='text-white md:text-2xl xl:text-5xl text-xl font-semibold'>GET IN TOUCH WITH US</h2>
-            <p data-aos="fade-right" className='text-white xl:text-xl md:text-sm text-sm'>We're here to help you with any questions or concerns</p>
+            <h2 data-aos="fade-right" className='text-white md:text-2xl xl:text-5xl text-xl font-semibold hover:text-[#777678]'>GET IN TOUCH WITH US</h2>
+            <p data-aos="fade-right" className='text-white xl:text-xl md:text-sm text-sm hover:text-[#777678]'>We're here to help you with any questions or concerns</p>
             <div className='grid xl:grid-cols-2 xl:gap-6 gap-6' data-aos="fade-right">
               <div className='flex items-start gap-3'>
                 <FaPhone className='text-black bg-white p-1 rounded-full size-8 md:mt-1 xl:mt-2 mt-0' />
                 <div>
-                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs'>Phone</h4>
-                  <p className='text-white xl:text-base md:text-sm text-xs'>+123-456-7890</p>
+                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs hover:text-[#777678]'>Phone</h4>
+                  <p className='text-white xl:text-base md:text-sm text-xs hover:text-[#777678]'>+123-456-7890</p>
                 </div>
               </div>
               <div className='flex items-start gap-3'>
                 <FaPhone className='text-black bg-white p-1 rounded-full size-8 md:mt-1 xl:mt-2 mt-0' />
                 <div>
-                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs'>Phone</h4>
-                  <p className='text-white xl:text-base md:text-sm text-xs'>+123-456-7890</p>
+                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs hover:text-[#777678]'>Phone</h4>
+                  <p className='text-white xl:text-base md:text-sm text-xs hover:text-[#777678]'>+123-456-7890</p>
                 </div>
               </div>
               <div className='flex items-start gap-3'>
                 <IoMdMail className='text-black bg-white p-1 rounded-full size-8 md:mt-1 xl:mt-2 mt-0' />
                 <div>
-                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs'>E-Mail</h4>
-                  <p className='text-white xl:text-base md:text-sm text-xs'>diamondatelierusa@gmail.com</p>
+                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs hover:text-[#777678]'>E-Mail</h4>
+                  <p className='text-white xl:text-base md:text-sm text-xs hover:text-[#777678]'>diamondatelierusa@gmail.com</p>
                 </div>
               </div>
               <div className='flex items-start gap-3'>
                 <SlGlobe className='text-black bg-white p-1 rounded-full size-8 md:mt-1 xl:mt-2 mt-0' />
                 <div>
-                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs'>Website</h4>
-                  <p className='text-white xl:text-base md:text-sm text-xs'>www.diamondatelier.com</p>
+                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs hover:text-[#777678]'>Website</h4>
+                  <p className='text-white xl:text-base md:text-sm text-xs hover:text-[#777678]'>www.diamondatelier.com</p>
                 </div>
               </div>
               <div className='flex items-start gap-3 md:col-span-2'>
                 <FaLocationDot className='text-black bg-white p-1 rounded-full size-8 md:mt-1 xl:mt-2 mt-0' />
                 <div>
-                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs'>Address</h4>
-                  <p className='text-white xl:text-base md:text-sm text-xs'>38 West 48th Street, 5th Floor, New York, NY 10036</p>
+                  <h4 className='text-white font-medium xl:text-base md:text-sm text-xs hover:text-[#777678]'>Address</h4>
+                  <p className='text-white xl:text-base md:text-sm text-xs hover:text-[#777678]'>38 West 48th Street, 5th Floor, New York, NY 10036</p>
                 </div>
               </div>
             </div>
